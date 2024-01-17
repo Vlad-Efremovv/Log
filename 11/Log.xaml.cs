65fab31be5f2c07a8f70b1f0f0f5864671ae1361
Ефремов Владислав Александрович <va.efremov@kscgroup.ru>
2024-01-17 07:03:57 +0300

    Добавьте файлы проекта.

---
+++ 11/Log.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _11
+{
+    /// <summary>
+    /// Логика взаимодействия для Log.xaml
+    /// </summary>
+    public partial class Log : Page
+    {
+        public Log()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Password;
+
+            string hashedPassword = PasswordHashing.CalculateMD5Hash(password);
+            //MessageBox.Show("Хэшированный пароль: " + hashedPassword);
+
+            /*if (YourAuthenticationService.ValidateUser(username, password))
+             {
+                 // Открываем новое окно или выполняем другие действия при успешной авторизации
+                 var mainAppWindow = new MainApplicationWindow();
+                 mainAppWindow.Show();
+                 this.Close();
+             }
+             else
+             {
+                 MessageBox.Show("Неверный логин или пароль");
+             }
+            */
+        }
+    }
+    public class PasswordHashing
+    {
+        public static string CalculateMD5Hash(string input)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("x2"));
+                }
+                return sb.ToString();
+            }
+        }
+    }
+}
---
+++ 11/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _11
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new Log());
+
+            //MenuFrameClass.MainFrame = MainFrame;                                           // присвоение фрейму параментров get set    
+
+            try
+            {
+                string connectionString = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=Больница;Integrated Security=True";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "База данных");
+            }
+        }
+    }
+}
